2c50a918195b358806e3b24958571c6947b8a878
le.vincent56@gmail.com <le.vincent56@gmail.com>
2022-08-22 17:37:58 -0400

    Added Card class

---
+++ Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    class Card
+    {
+        // Fields
+        protected string name;
+        protected int manaCost;
+        protected string description;
+
+        // Properties
+        public string Name { get { return name; } }
+        public int ManaCost { get { return manaCost; } set { manaCost = value; } }
+        public string Description { get { return description; } }
+
+        // Methods
+        public void Play()
+        {
+
+        }
+
+        public void Discard()
+        {
+
+        }
+    }
+}

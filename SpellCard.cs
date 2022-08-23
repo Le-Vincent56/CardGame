301528f825b85923263690e54b64e9d585ea7fcd
Crytonical <Brewster0503@gmail.com>
2022-08-23 00:41:18 -0400

    Worked on spell class

---
+++ SpellCard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame
+{
+    public enum CardType
+    {
+        Damage,
+        Buff,
+        Control,
+        Heal
+    }
+
+    class SpellCard : Card
+    {
+        // Fields 
+        private CardType spellType;
+        private int flexNumber;
+
+        // Constructor        
+        public SpellCard(string name, int manaCost, string description, int type, int flexNumber)
+            : base(name, manaCost, description)
+        {
+            spellType = (CardType)type;
+            this.flexNumber = flexNumber;
+        }
+
+        // Methods
+        private void CheckSpellType(CardType type)
+        {
+            switch (type)
+            {
+                case CardType.Damage:
+
+                    break;
+
+                case CardType.Buff:
+
+                    break;
+
+                case CardType.Control:
+
+                    break;
+
+                case CardType.Heal:
+
+                    break;
+            }
+        }
+
+        public void Play(CardType type, )
+        {
+
+            switch (type)
+            {
+                case CardType.Damage:
+                    if (target is MinionCard)
+                    {
+                        (MinionCard)target
+                    }
+                    break;
+
+                case CardType.Buff:
+
+                    break;
+
+                case CardType.Control:
+
+                    break;
+
+                case CardType.Heal:
+
+                    break;
+            }
+        }
+
+        public void SelectTarget()
+        {
+
+        }
+    }
+}
+
